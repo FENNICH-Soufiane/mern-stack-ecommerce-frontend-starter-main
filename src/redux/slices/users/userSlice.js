0c475adce6794bd5cd7fa435c5848796c10236fe
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-07 16:58:06 +0100

    Login Action

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,12 +1,38 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
 const initialState = {
-    loading: false,
-    error: null,
-    users: [],
-    user: null,
-    profile: {},
-    userAuth: {
+   loading: false,
+   error: null,
+   users: [],
+   user: null,
+   profile: {},
+   userAuth: {
       loading: false,
       error: null,
       userInfo: {}
-    },
-  };
+   },
+};
+
+
+//register action
+export const registerUserAction = createAsyncThunk(
+   "users/register",
+   async (
+      { email, password, fullname },
+      { rejectWithValue, getState, dispatch }
+   ) => {
+      try {
+         //make the http request
+         const { data } = await axios.post(`${baseURL}/users/register`, {
+            email,
+            password,
+            fullname,
+         });
+         return data;
+      } catch (error) {
+         console.log(error);
+         return rejectWithValue(error?.response?.data);
+      }
+   }
+);
