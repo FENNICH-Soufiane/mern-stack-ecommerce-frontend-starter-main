eb4c41fd4295f3e4c454ef51638f7718fe11a5a7
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-07 17:01:29 +0100

    Users Slice

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -36,3 +36,24 @@ export const registerUserAction = createAsyncThunk(
       }
    }
 );
+
+
+const usersSlice = createSlice({
+   name: "users",
+   initialState,
+   extraReducers: (builder) => {
+      //handle actions
+      //login
+      builder.addCase(loginUserAction.pending, (state, action) => {
+         state.userAuth.loading = true;
+      });
+      builder.addCase(loginUserAction.fulfilled, (state, action) => {
+         state.userAuth.userInfo = action.payload;
+         state.userAuth.loading = false;
+      });
+      builder.addCase(loginUserAction.rejected, (state, action) => {
+         state.userAuth.error = action.payload;
+         state.userAuth.loading = false;
+      });
+   }
+});
