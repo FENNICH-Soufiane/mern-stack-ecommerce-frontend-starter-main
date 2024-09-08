f38164cc6c53ea6a9db9e82420a27991a916e08d
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-08 15:21:23 +0100

    Display Error Message When Login

--- src/components/Users/Forms/Login.js
+++ src/components/Users/Forms/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUserAction } from "../../../redux/slices/users/userSlice";
+import ErrorMsg from "../../ErrorMsg/ErrorMsg";
 
 
 const Login = () => {
