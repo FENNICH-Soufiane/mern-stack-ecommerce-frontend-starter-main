14337afe86ebe8778426f8dd6739b719731c8d40
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-09 19:10:40 +0100

    Reset Error And Success Actions

--- src/components/ErrorMsg/ErrorMsg.js
+++ src/components/ErrorMsg/ErrorMsg.js
@@ -1,11 +1,17 @@
+import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
+import { resetErrAction } from "../../redux/slices/users/globalActions/globalActions";
+
 
 const ErrorMsg = ({ message }) => {
+  const dispatch = useDispatch();
   Swal.fire({
     icon: "error",
     title: "Oops...",
     text: message,
+    confirmButtonColor: "red"
   });
+  dispatch(resetErrAction());
 };
 
 export default ErrorMsg;
--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import baseURL from "../../../utils/baseURL";
+import { resetErrAction } from "./globalActions/globalActions";
 
 const initialState = {
    loading: false,
@@ -89,6 +90,10 @@ const usersSlice = createSlice({
       builder.addCase(registerUserAction.rejected, (state, action) => {
          state.error = action.payload;
          state.loading = false;
+      });
+      //reset error action
+      builder.addCase(resetErrAction.pending, (state) => {
+         state.error = null;
       });
    }
 });
