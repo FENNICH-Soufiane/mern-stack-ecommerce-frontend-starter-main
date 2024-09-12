11d4f3852749d69f0a4cc40764baa095794a7226
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-12 15:56:57 +0100

    Modified Add Category Form

--- src/components/Admin/Categories/AddCategory.js
+++ src/components/Admin/Categories/AddCategory.js
@@ -13,6 +13,29 @@ export default function CategoryToAdd() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   let { error, isAdded, loading } = {};
+
+
+  // files
+  const [files, setFiles] = useState([]);
+  const [fileErrors, setFilesErros] = useState([]);
+  const fileHandleChange = (event) => {
+    const newFiles = Array.from(event.target.files);
+    console.log(newFiles);
+
+    //validation
+    const newErrs = [];
+    newFiles.forEach((file) => {
+      if (file?.size > 1000000) {
+        newErrs.push(`${file?.name} is too large`);
+      }
+      if (!file?.type?.startsWith("image/")) {
+        newErrs.push(`${file?.name} is not an image`);
+      }
+    });
+    setFiles(newFiles);
+    setFilesErros(newErrs)
+    console.log(newFiles);
+  }
   //onSubmit
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -59,7 +82,46 @@ export default function CategoryToAdd() {
                   />
                 </div>
               </div>
+              {/* upload images */}
+              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                <div className="mt-1 sm:col-span-2 sm:mt-0">
+                  <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true">
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+                          <span>Upload file</span>
+                          <input
+                            onChange={fileHandleChange}
+                            name="files"
+                            type="file"
+                            multiple
+                          />
+                        </label>
+                      </div>
+                      <p className="text-xs text-gray-500">
+                        PNG, JPG, GIF up to 1MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div>
+
                 {loading ? (
                   <LoadingComponent />
                 ) : (
