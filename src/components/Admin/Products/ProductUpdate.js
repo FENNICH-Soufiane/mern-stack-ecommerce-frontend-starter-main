d9671277d542838d06edb528dce83dd1b7150576
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-23 13:09:01 +0100

    Modified Update Product form

--- src/components/Admin/Products/ProductUpdate.js
+++ src/components/Admin/Products/ProductUpdate.js
@@ -71,28 +71,8 @@ export default function ProductUpdate() {
     };
   });
 
-  // files
-  const [files, setFiles] = useState([]);
-  const [fileErrors, setFilesErros] = useState([]);
-  // file handle change
-  const fileHandleChange = (event) => {
-    const newFiles = Array.from(event.target.files);
-    console.log(newFiles);
-    
-    //validation
-    const newErrs = [];
-    newFiles.forEach((file) => {
-      if (file?.size > 1000000) {
-        newErrs.push(`${file?.name} is too large`);
-      }
-      if (!file?.type?.startsWith("image/")) {
-        newErrs.push(`${file?.name} is not an image`);
-      }
-    });
-    setFiles(newFiles);
-    setFilesErros(newErrs)
-    console.log(newFiles);
-  }
+  
+  
 
   //---form data---
   const [formData, setFormData] = useState({
@@ -116,12 +96,10 @@ export default function ProductUpdate() {
   //onSubmit
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(fileErrors);
     
     // dispatch
     dispatch(createProductAction({
       ...formData,
-      files,
       colors: colorsOption?.map((color) => color?.label),
       sizes: sizeOption?.map((size) => size?.label),
     }));
@@ -142,9 +120,7 @@ export default function ProductUpdate() {
 
   return (
     <>
-      {error && <ErrorMsg message={error?.message} />}
       {isAdded && <SuccessMsg message="Product Added Successfully" />}
-      {fileErrors.length >0 && <ErrorMsg message="file too large or upload an image" />}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
@@ -255,49 +231,7 @@ export default function ProductUpdate() {
                 />
               </div>
 
-              {/* upload images */}
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                <label
-                  htmlFor="cover-photo"
-                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                  Upload Images
-                </label>
-                <div className="mt-1 sm:col-span-2 sm:mt-0">
-                  <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true">
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                          <span>Upload files</span>
-                          <input
-                            onChange={fileHandleChange}
-                            name="files"
-                            type="file"
-                            multiple
-                          />
-                        </label>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF up to 1MB
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
 
               {/* price */}
               <div>
@@ -352,10 +286,10 @@ export default function ProductUpdate() {
                   <LoadingComponent />
                 ) : (
                   <button
-                  disabled={fileErrors.length >0}
+                  disabled
                     type="submit"
                     className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Add Product
+                    Update Product
                   </button>
                 )}
               </div>
